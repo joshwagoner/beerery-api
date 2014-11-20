9ec966f1f3a14ce7004734c580e544607e1d65c9
Josh Wagoner <wagoner.josh@gmail.com>
2014-11-19 22:42:26 -0800

    enabling cross domain

--- app.js
+++ app.js
@@ -66,9 +66,18 @@ function updateConfigItem(inputId, path, newConfig, res){
 	});
 }
 
+var allowCrossDomain = function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+
+    next();
+};
+
 var app = express();
 
 app.set('port', process.env.PORT || 3000);
+app.use(allowCrossDomain);
 app.use(bodyParser.json());
 
 app.get('/', function(req, res) {
